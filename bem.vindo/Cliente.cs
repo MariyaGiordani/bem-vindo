f59f008b3eeace243c58bf523fcdf04f79cefa89
Mariya Lukanova <mariya.lukanova@iwsbrazil.com>
2018-06-07 13:47:25 -0300

    Correct branch

--- bem.vindo/Cliente.cs
+++ bem.vindo/Cliente.cs
@@ -8,5 +8,26 @@ namespace bem.vindo
 {
     class Cliente
     {
+        public int CodigoDoCliente { get; set; }
+        public String Nome { get; set; }
+        public int Idade { get; set; }
+        public String EstadoCivil { get; set; }
+        public String Genero { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="codigoDoCliente"></param>
+        /// <param name="nome"></param>
+        /// <param name="idade"></param>
+        /// <param name="estadoCivil"></param>
+        /// <param name="genero"></param>
+        public Cliente(int codigoDoCliente, String nome, int idade, String estadoCivil, String genero)
+        {
+            CodigoDoCliente = codigoDoCliente;
+            Nome = nome;
+            Idade = idade;
+            EstadoCivil = estadoCivil;
+            Genero = genero;
+        }
     }
 }
