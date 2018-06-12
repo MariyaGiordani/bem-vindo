4eb6474c2eb1abebbe9c084d0d02c3477cad930f
Mariya Lukanova <mariya.lukanova@iwsbrazil.com>
2018-06-12 09:28:39 -0300

    New 2

--- bem.vindo/Program.cs
+++ bem.vindo/Program.cs
@@ -20,27 +20,56 @@ namespace bem.vindo
                 cliente.CodigoDoCliente = Convert.ToInt32(Console.ReadLine().ToString());
 
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("Informação digitada está errada!!!");
+                Console.WriteLine("Codigo com erro!");
+                Console.WriteLine("Mensagem de erro:" + ex.Message);
             }
-            Console.WriteLine("Digite o nome do cliente: ");
-            cliente.Nome = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Digite o nome do cliente: ");
+                cliente.Nome = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Nome está com erro!");
+                Console.WriteLine("Mensagem de erro:" + ex.Message);
+            }
             try
             {
                 Console.WriteLine("Digite o idade do cliente: ");
                 cliente.Idade = Convert.ToInt32(Console.ReadLine().ToString());
 
             }
-            catch
+            catch (Exception ex)
             {
-                Console.WriteLine("Informação digitada está errada!!!");
-                
+                Console.WriteLine("Idade está com errado!");
+                Console.WriteLine("Mensagem de erro:" + ex.Message);
+
             }
-            Console.WriteLine("Digite estado civil do cliente: ");
-            cliente.EstadoCivil = Console.ReadLine();
-            Console.WriteLine("Digite o genero do cliente: ");
-            cliente.Genero = Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Digite estado civil do cliente: ");
+                cliente.EstadoCivil = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Estado civil está com errado!");
+                Console.WriteLine("Mensagem de erro:" + ex.Message);
+            }
+            try
+            {
+                Console.WriteLine("Digite o genero do cliente: ");
+                cliente.Genero = Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Genero está com errado!");
+                Console.WriteLine("Mensagem de erro:" + ex.Message);
+            }
             Console.Clear();
 
 
@@ -49,16 +78,61 @@ namespace bem.vindo
             {
                 Endereco endereco = new Endereco();
                 endereco.CodigoDoCliente = cliente.CodigoDoCliente;
-                Console.WriteLine("Digite rua:");
-                endereco.Rua = Console.ReadLine();
-                Console.WriteLine("Digite complemento:");
-                endereco.Complemento = Console.ReadLine();
-                Console.WriteLine("Digite CEP:");
-                endereco.CEP = Console.ReadLine();
-                Console.WriteLine("Digite bairro:");
-                endereco.Bairro = Console.ReadLine();
-                Console.WriteLine("Digite cidade:");
-                endereco.Cidade = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine("Digite rua:");
+                    endereco.Rua = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Rua está com errado!");
+                    Console.WriteLine("Mensagem de erro:" + ex.Message);
+                }
+                try
+                {
+                    Console.WriteLine("Digite complemento:");
+                    endereco.Complemento = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Complemento está com errado!");
+                    Console.WriteLine("Mensagem de erro:" + ex.Message);
+                }
+                try
+                {
+                    Console.WriteLine("Digite CEP:");
+                    endereco.CEP = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("CEP está com errado!");
+                    Console.WriteLine("Mensagem de erro:" + ex.Message);
+                }
+                try
+                {
+                    Console.WriteLine("Digite bairro:");
+                    endereco.Bairro = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Bairro está com errado!");
+                    Console.WriteLine("Mensagem de erro:" + ex.Message);
+                }
+                try
+                {
+                    Console.WriteLine("Digite cidade:");
+                    endereco.Cidade = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine("Cidade está com errado!");
+                    Console.WriteLine("Mensagem de erro:" + ex.Message);
+                }
                 cliente.lista.Add(endereco);
                 Console.Clear();
 
